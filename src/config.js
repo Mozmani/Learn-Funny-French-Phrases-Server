bb0f6e10e080737185d0b2dc5b1102a95220fd0a
Mark <SwiftTactics@gmail.com>
2020-11-11 19:03:42 -0500

    port issues

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: process.env.PORT || 8000,
+  PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL
     || 'postgresql://dunder-mifflin@localhost/spaced-repetition',
